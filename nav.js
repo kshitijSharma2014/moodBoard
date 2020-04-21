fbbd9f0d7b7d1eceb7436bddc20ba2d05a273990
kshitij sharma <ksh.anand2012@gmail.com>
2020-04-21 10:45:52 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './navigationBar.css';
 import LocationSelectBox from '../LocationSelectBox';
 import _get from 'lodash/get';
+import _findIndex from 'lodash/findIndex';
+import _map from 'lodash/map';
 import axios from 'axios';
 import ConfirmationBox from '../ConfirmationBox';
 import AlertBox from '../AlertBox';
@@ -14,6 +15,36 @@ class NavigationBar extends React.PureComponent {
     selectedLocation: null,
     showInfoBox: false,
     toggleConfirmation: false,
+    sites: [],
+  }
+
+  componentDidMount() {
+    axios.get(`https://jiobeatplanner.st.ril.com:8082/api/plm/getPLMList?phoneno=${this.props.phoneno}`)
+      .then(res => {
+        const sites = this.formatOptions(res);
+        this.setState({ sites });
+      })
+  }
+
+  formatOptions = (response) => {
+    const res = _get(response, 'data', []);
+    const options = _map(res, (item)=> {
+      return {
+        value: item.siteid,
+        label: item.type,
+        verified: item.isprocessed === "1" ? true : false,
+      }
+    });
+    return options;
+  }
+
+  updateSites = (verifiedSite) => {
+    const newState = Object.assign({}, this.state.sites);
+    const verifiedSiteIndex = _findIndex(newState, function(site) { return site.value === verifiedSite.value; })
+    if (verifiedSiteIndex) {
+      newState[verifiedSiteIndex].verified = true;
+    }
+    this.setState({sites: newState})
   }
 
   changeSelectedLocation = (selectedLocation) => {
@@ -22,12 +53,10 @@ class NavigationBar extends React.PureComponent {
 
   verifySelectedLocation = () => {
     const payload = {
-      usrdata: {
         lat: _get(this, 'props.position.lat', '').toString(),
         lon: _get(this, 'props.position.lng', '').toString(),
         siteid: _get(this, 'state.selectedLocation.value', ''),
         phoneno: _get(this, 'props.phoneno', ''),
-      }
     }
 
     this.toggleConfirmationBox();
@@ -37,32 +66,16 @@ class NavigationBar extends React.PureComponent {
         showAlertBox: true,         
         AlertText: 'Site verification Successfull!!',         
         alertType: 'success',       
-      })             
-    });
-    // .fail(() => {
-    //   that.setState({
-    //     showAlertBox: true,
-    //     AlertText: 'Site verification failed!!',
-    //     alertType: 'failure',
-    //   })
-    // })
-    // axios.post('https://jiobeatplanner.st.ril.com:8082/api/plm/VerifyPLMUserLoc', payload, config)
-    //   .then(res => {
-    //     console.log(res);
-    //     console.log(res.data);
-    //     this.setState({
-    //       showAlertBox: true,
-    //       AlertText: 'Site verification Successfull!!',
-    //       alertType: 'success',
-    //     })
-    //   })
-    //   .catch(() => {
-    //     this.setState({
-    //       showAlertBox: true,
-    //       AlertText: 'Site verification failed!!',
-    //       alertType: 'failure',
-    //     })
-    //   })
+      })       
+      this.updateSites(_get(that, 'state.selectedLocation'));      
+    })
+    .fail(() => {
+      that.setState({
+        showAlertBox: true,
+        AlertText: 'Site verification failed!!',
+        alertType: 'failure',
+      })
+    })
   }
 
   closeAlertBox = () => {
@@ -87,20 +100,21 @@ class NavigationBar extends React.PureComponent {
       <div className="df fc">
         <div className="navHd">
           <div className="header_title">Site Locator</div>
-          <img src={require('./jioLogo.png')} className="jioLogoImg" />
+          <img alt="jioLogo" src={require('./jioLogo.png')} className="jioLogoImg" />
         </div>
         <div className="df justifyContentCentre alignItemsCentre m-t-12 m-b-12">
           <LocationSelectBox
             selectedLocation={this.state.selectedLocation}
             changeSelectedLocation={this.changeSelectedLocation}
             phoneno={this.props.phoneno}
+            sites={this.state.sites}
           />
           <button className="btn btn-sm btn-primary bVerify" id="bVerify" onClick={this.toggleConfirmationBox}>Verify</button>
         </div>
-        <img src={require("./info.svg")} className="infoIconImg" onClick={this.toggleInfoBox} />
+        <img alt="info" src={require("./info.svg")} className="infoIconImg" onClick={this.toggleInfoBox} />
         {this.state.showInfoBox &&
           <div className="df fc infoData">
-            <img src={require("./close.svg")} className="closeSvg pa" onClick={this.hideInfoBox} />
+            <img alt="close" src={require("./close.svg")} className="closeSvg pa" onClick={this.hideInfoBox} />
             <div>
               Instructions for Partner Location Verrification
             </div>
