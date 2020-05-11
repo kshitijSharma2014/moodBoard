06d6de28dafb42e0f88f8aa637bbe93f1d346855
kshitij sharma <ksh.anand2012@gmail.com>
2020-05-11 16:19:08 +0530

    Update util.js

--- util.js
+++ util.js
@@ -36,34 +36,25 @@ function onDateFilterApply() {
             closeSnoDangerAlertBox();
         }
     }
+<script>
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+   window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+    </script>
 
 
-.otpResend {
-  text-align: center;
-  color: #ffffff;
-  margin-left: auto;
-  margin-right: auto;
-  bottom: 50px;
-  margin-top: 50px;
+
+
+
+
+.mapContainer {
+  height: calc(100vh - 120px);
+  height: calc((var(--vh, 1vh) * 100) - 120px);
   width: 100%;
-  text-decoration: underline;
 }
-
-.titleContainer {
-    height: 170px;
-    padding: 20px;
-    color: grey;
-}
-
-
-.boxbtnGrp {
-    align-items: center;
-    justify-content: flex-end;
-    padding: 10px;
-    border-top: 1px solid #e9ecef;
-    height: 30px;
-}
-
-.confirmationBoxContainer -> 260px
-
-confirmationbox.js df fc
